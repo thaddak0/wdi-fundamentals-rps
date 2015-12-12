48bb5512156d914db0cb7fe7f7c6907d1dcd1ff0
Annabelle Thaddeus <annabellethaddeus@Annabelles-MBP.attlocal.net>
2015-12-12 15:02:07 -0600

    get winner expressions

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -25,6 +25,7 @@ function getPlayerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `getInput()`.
+    
     return move;
 }
 
@@ -36,11 +37,36 @@ function getComputerMove(move) {
 }
 
 function getWinner(playerMove,computerMove) {
-    var winner;
+    var winner =
+
+    if (playerMove === computerMove) {
+        return 'tie';
+
+    }   else if ((playerMove === 'rock') && (computerMove === 'paper')) {
+        return 'computer';
+
+    }   else if ((playerMove === 'rock') && (computerMove === 'scissors')) {
+        return 'player';
+
+    }   else if ((playerMove === 'paper') && (computerMove === 'rock')) {
+        return 'player';
+
+    }   else if ((playerMove === 'paper') && (computerMove === 'scissors')) {
+        return 'computer';
+
+    }   else if ((playerMove === 'scissors') &&& (computerMove === 'rock')) {
+        return 'computer';
+
+    }   else if ((playerMove === 'scissors') &&& (computerMove === 'paper')) {
+        return 'player';
+
+    }
+
     // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
     /* YOUR CODE HERE */
+
     return winner;
 }
 
